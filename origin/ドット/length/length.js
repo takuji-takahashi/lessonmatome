38cef9c2d6c3e7ad422d84be15a7af3654dc312b
takuji-takahashi <you@example.com>
2020-06-18 16:19:08 +0900

    5

---
+++ origin/ドット/length/length.js
@@ -0,0 +1,23 @@
+'use strict'
+
+//DOM
+const comment = document.getElementById('comment');
+const label = document.getElementById('label');
+
+const LIMIT = 500;
+const WARNING = 10;
+
+label.innerHTML = LIMIT;
+
+comment.addEventListener('keyup', function () {
+
+    let remaining = LIMIT - this.value.length;
+    label.innerHTML = remaining;
+    // if (remaining < WARNING) {
+    //     label.className = 'warning';
+    // } else {
+    //     label.className = '';
+    // }
+    label.className = remaining < WARNING ? 'warning' : '';
+});
+
